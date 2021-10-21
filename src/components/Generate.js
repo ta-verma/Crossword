9b6c058310cb806c68ead435fc3a2fd6957c5759
Deepak Singh Kushwaha <d0020002@gmail.com>
2021-10-21 23:57:26 +0530

    add timer

--- src/components/Generate.js
+++ src/components/Generate.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react'
 import { DrawCrossWord } from './drawCrossword/draw'
 import jsPdf from 'jspdf'
 import domtoimage from 'dom-to-image';
-
+import Timer from './timer';
 
 export default function Generate() {
     const [data, setdata] = useState("")
+    const [isclicked, setclicked] = useState(false);
     // const [wordList, setWordList] = useState([])
+
 
     const generate = () => {
         function csvToArray(csv) {
@@ -20,11 +22,13 @@ export default function Generate() {
         // setWordList(wordList)
         // var result = Crossword(wordList)
         DrawCrossWord(wordList)
-
+        setclicked(!isclicked);
+        
     }
 
     const generateRandom = () => {
         DrawCrossWord()
+        setclicked(!isclicked);
     }
 
 
@@ -51,20 +55,46 @@ export default function Generate() {
             document.querySelector(".crossword-buttons").style.visibility = 'visible'
         });
     }
+
+    const newCrossWord = () => {
+        const crswrd = document.querySelector(".crossword");
+        crswrd.innerHTML = '';
+        setclicked(!isclicked)
+    }
+
+
     return (
         <>
             <div class="container" style={{ paddingTop: "300px", paddingBottom: "300px" }}>
-                <label htmlFor="txtarea" class="mx-auto"> Words And Clue</label>
-                <textarea class="form-control" onChange={(e) => { setdata(e.target.value) }} placeholder="" rows="10" cols="50" id="txtarea"></textarea>
-                <button onClick={generate} class="btn btn-outline-primary mt-5 mx-auto" type="button">
-                    Generate
-                </button>
-                <button onClick={generateRandom} class="btn btn-outline-primary mx-5 mt-5" type="button">
-                    Generate Random
-                </button>
+                
+
+                {!isclicked ?  
+                    <>
+                        <label htmlFor="txtarea" class="mx-auto"> Words And Clue</label>
+                        <textarea class="form-control" onChange={(e) => { setdata(e.target.value) }} placeholder="" rows="10" cols="50" id="txtarea"></textarea>
+                        <button onClick={generate} class="btn btn-outline-primary mt-5 mx-auto" type="button">
+                            Generate
+                        </button>
+                        <button onClick={generateRandom} class="btn btn-outline-primary mx-5 mt-5" type="button">
+                            Generate Random
+                        </button>
+                    </> 
+                    : 
+                    <>
+                        <Timer />
+                        <button onClick = {newCrossWord} class="btn btn-outline-primary mx-5 mt-5" type="button">
+                            New CrossWord
+                        </button>
+                    </>
+                    }
+                
+
+
+
                 <button onClick={generatePDF} class="btn btn-outline-primary mx-5 mt-5" type="button">
                     save as PDF
                 </button>
+                
                 <div>
                     <div class="crossword"> </div>
                 </div>
