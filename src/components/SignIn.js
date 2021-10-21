c000bffa1a725ce6ba60d0e036fa071722085fe7
Tarun Verma <34982557+ta-verma@users.noreply.github.com>
2021-10-21 11:33:33 +0530

    form validation added

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,38 +1,74 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import Recaptcha from 'react-recaptcha'
+import GoogleLogin from 'react-google-login'
 
 export default function SignIn() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [success, setSuccess] = useState(false)
+    const [message, setMessage] = useState('')
+    const [isRobot, setIsRobot] = useState(true)
 
     axios.defaults.withCredentials = true;
 
     const login = (e) => {
         e.preventDefault()
-        axios.post("http://localhost:3001/signin", {
-            username: username,
-            password: password
-        }).then((response) => {
-            if (response.data.message) {
-                console.log(response.data.message);
-            } else {
-                // console.log(response)
-                // setLoginStatus(response.data[0].USERNAME);
-                localStorage.setItem("isAuthenticated", "true");
-                window.location.pathname = "/dashboard";
-            }
-        });
-    };
+        if (username !== "" && password !== "") {
+            if (!isRobot)
+                axios.post("https://crossw-server.herokuapp.com/signin", {
+                    username: username,
+                    password: password
+                }).then((response) => {
+                    if (response.data.message) {
+                        // console.log(response.data.message);
+                        setSuccess(true)
+                        setMessage(response.data.message)
 
+                    } else {
+                        localStorage.setItem("isAuthenticated", "true");
+                        window.location.pathname = "/dashboard";
+                    }
+                });
+            else {
+                setSuccess(true)
+                setMessage("please verify the captcha")
+            }
+        }
+        else {
+            setSuccess(true)
+            setMessage("username/password can't be empty")
+        }
+    }
+    const toggleBot = () => {
+        if (isRobot)
+            setIsRobot(false)
+        else
+            setIsRobot(true)
+    }
+
+    const responseGoogle = (res) => {
+        axios.post("https://crossw-server.herokuapp.com/googlesignup", {
+            googleId: res.profileObj['googleId'],
+            name: res.profileObj['name'],
+            email: res.profileObj['email']
+        }).then((response) => {
+            localStorage.setItem("isAuthenticated", "true");
+            window.location.pathname = "/dashboard"
+        })
+    }
+    const responseFailGoogle = (res) => {
+        setSuccess(true)
+        setMessage("Sign in attempt failed !")
+    }
     useEffect(() => {
         const isAuthenticated = localStorage.getItem("isAuthenticated");
         if (isAuthenticated) {
             window.location.pathname = "/dashboard";
         }
-        axios.get("http://localhost:3001/signin").then((response) => {
+        axios.get("https://crossw-server.herokuapp.com/signin").then((response) => {
             if (response.data.loggedIn === true) {
-                // setLoginStatus(true)
             }
         });
     }, []);
@@ -42,9 +78,6 @@ export default function SignIn() {
             <div className="container py-5 h-100">
 
                 <div className="row d-flex align-items-center justify-content-center h-100">
-                    {/* <div className="col-md-8 col-lg-7 col-xl-6">
-              <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-login-form/draw2.svg" className="img-fluid" alt="Phone image"/>
-            </div> */}
 
                     <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
                         <h3 className="py-4">  Sign In to you Account</h3>
@@ -59,14 +92,27 @@ export default function SignIn() {
                             <div className="form-outline mb-4">
                                 <input type="password" id="form1Example23" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" className="form-control form-control-lg" />
                             </div>
-
+                            {success ? <>
+                                <div id="alert" className="mt-4 text-center alert alert-danger" role="alert">
+                                    {message}
+                                </div>
+                            </> : <></>}
+                            <div className="form-outline d-flex justify-content-center mb-4">
+                                <Recaptcha
+                                    sitekey="6Lc2rOMcAAAAADZMnatG2s4hlU_gMCNbkEZozThP"
+                                    render="explicit"
+                                    verifyCallback={toggleBot}
+                                    expiredCallback={toggleBot}
+                                    onloadCallback={() => { }}
+                                />
+                            </div>
                             <div className="d-flex flex-row-reverse mb-4">
                                 <a href="#!">Forgot password?</a>
                             </div>
 
                             {/* <!-- Submit button --> */}
                             <div className="d-flex justify-content-center ">
-                                <button type="submit" onClick={login}  className="btn btn-primary btn-lg btn-block">Sign in</button>
+                                <button type="submit" onClick={login} className="btn btn-primary btn-lg btn-block">Sign in</button>
                             </div>
                             <div className="d-flex justify-content-center align-items-center my-4">
                                 <p className="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
@@ -74,72 +120,21 @@ export default function SignIn() {
 
 
                             <div className="d-flex justify-content-center" >
-                                <a className="btn btn-outline-dark" href="/users/googleauth" role="button" style={{ textTransform: "none" }}>
+                                <GoogleLogin clientId="423957503910-f9jtmoks9tlkusbi3gurkbgp9ul87gmv.apps.googleusercontent.com" buttonText="Continue with google"
+                                    onSuccess={responseGoogle} onFailure={responseFailGoogle} cookiePolicy={'single_host_origin'} />
+                                {/* <a className="btn btn-outline-dark" href="/users/googleauth" role="button" style={{ textTransform: "none" }}>
                                     <img width="20px" alt="Google sign-in" className="mx-2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
                                     Login with Google
-                                </a>
+                                </a> */}
+
                             </div>
                             <div className="d-flex justify-content-center my-3">
-                            <Link to="/signup" class="link-dark">You don&#x27;t have an account?</Link>
+                                <Link to="/signup" className="link-dark">You don&#x27;t have an account?</Link>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
         </section>
-
-        // <div className="container py-24 m-16 mx-auto self-center flex flex-col justify-center items-center">
-        //     <div className="flex flex-col w-full max-w-md px-4 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
-        //         <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-white">
-        //             Login To Your Account
-        //         </div>
-        //         <div className="flex gap-4 item-center">
-        //             <button type="button" className="py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-        //                 <svg width="20" height="20" fill="currentColor" className="mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-        //                     <path d="M1343 12v264h-157q-86 0-116 36t-30 108v189h293l-39 296h-254v759h-306v-759h-255v-296h255v-218q0-186 104-288.5t277-102.5q147 0 228 12z">
-        //                     </path>
-        //                 </svg>
-        //                 Facebook
-        //             </button>
-        //             <button type="button" className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-        //                 <svg width="20" height="20" fill="currentColor" className="mr-2" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-        //                     <path d="M896 786h725q12 67 12 128 0 217-91 387.5t-259.5 266.5-386.5 96q-157 0-299-60.5t-245-163.5-163.5-245-60.5-299 60.5-299 163.5-245 245-163.5 299-60.5q300 0 515 201l-209 201q-123-119-306-119-129 0-238.5 65t-173.5 176.5-64 243.5 64 243.5 173.5 176.5 238.5 65q87 0 160-24t120-60 82-82 51.5-87 22.5-78h-436v-264z">
-        //                     </path>
-        //                 </svg>
-        //                 Google
-        //             </button>
-        //         </div>
-        //         <div className="mt-8">
-        //             <form className="space-y-5">
-        //                 <div>
-        //                     <input type="username" placeholder="Username" onChange={(e) => { setUsername(e.target.value) }} className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-blue-500" />
-        //                 </div>
-
-        //                 <div>
-        //                     <input type="password" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-blue-500" />
-        //                 </div>
-
-        //                 <div className="flex items-center mb-6 -mt-4">
-        //                     <div className="flex ml-auto">
-        //                         <a href="/" className="inline-flex text-xs text-gray-700 sm:text-sm dark:text-gray-100 hover:text-gray-900 dark:hover:text-white">
-        //                             Forgot Your Password?
-        //                         </a>
-        //                     </div>
-        //                 </div>
-
-        //                 <div>
-        //                     <button onClick={login} className="block w-full bg-blue-500 hover:bg-blue-700 p-4 rounded text-white hover:text-while transition duration-200">Login</button>
-        //                 </div>
-        //             </form>
-        //         </div>
-        //         <div className="flex items-center justify-center my-6">
-        //             <Link to="/signup" className="inline-flex items-center text-xs  text-center text-gray-700 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white">
-        //                 <span className="ml-2">
-        //                     You don&#x27;t have an account?
-        //                 </span>
-        //             </Link>
-        //         </div>
-        //     </div>
-        // </div>
     )
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,32 +1,93 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from "react-router-dom"
+import Recaptcha from 'react-recaptcha'
+
 
 export default function SignUp() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [confPassword, setConfPassword] = useState('')
     const [user, setUser] = useState('')
     const [email, setEmail] = useState('')
     const [success, setSuccess] = useState(false)
     const [message, setMessage] = useState('')
+    const [isSuccess, setIsSuccess] = useState(true)
+    const [isRobot, setIsRobot] = useState(true)
+
+    const validate = (e) => {
+
+        var name_e = /^[A-Za-z]+[A-Za-z ]*[A-Za-z]$/;
+        var username_e = /^([A-Za-z_]+[A-Za-z0-9_]*){1,}/;
+        var password_e = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/;
+
+
+        if (!user.match(name_e))
+            return "Are you sure you entered your name correctly?"
+        else if (!username.match(username_e))
+            return "Username must starts with a Alphabet or underscore"
+        else if (email === "")
+            return "Email Can't be empty"
+        else if (!password.match(password_e))
+            return "Password must include a uppercase, lowercase, number and special character"
+        else if (password !== confPassword)
+            return "Passwords do not match"
+        else if (!document.getElementById("form2Example3g").checked)
+            return "Please agree to terms and privacy"
+        else if (isRobot)
+            return "Please verify the captcha"
+        else
+            return "ok"
+    }
 
     const register = (e) => {
         e.preventDefault()
-        axios.post("http://localhost:3001/signup", {
-            username: username,
-            user: user,
-            email: email,
-            password: password
-        }).then((response) => {
-            setMessage(response.data.message)
+        const sec = validate(e)
+        if (sec === "ok") {
+            axios.post("https://crossw-server.herokuapp.com/signup", {
+                username: username,
+                user: user,
+                email: email,
+                password: password
+            }).then((response) => {
+                var msg = response.data.message
+                if(msg === "ok"){
+                    setMessage("Account Created Successfully !")
+                    document.getElementById("signLink").innerHTML = "Click here to Sign in"
+                    setIsSuccess(true)
+                    setSuccess(true)
+                    document.getElementById("signupForm").reset()
+                }
+                else{
+                    setMessage(msg)
+                    setIsSuccess(false)
+                    setSuccess(true)
+                }
+            }).catch((err) => {
+                console.log(err)
+            });
+        }
+        else {
+            setMessage(sec)
+            setIsSuccess(false)
             setSuccess(true)
-            document.getElementById("signupForm").reset()
-        }).catch((err) => {
-            console.log(err)
-        });
-        // alert()
+        }
     }
 
+  
+    const toggleBot = () => {
+        isRobot ? setIsRobot(false):setIsRobot(true)
+    }
+    useEffect(() => {
+        const isAuthenticated = localStorage.getItem("isAuthenticated");
+        if (isAuthenticated) {
+            window.location.pathname = "/dashboard";
+        }
+        axios.get("https://crossw-server.herokuapp.com/signin").then((response) => {
+            if (response.data.loggedIn === true) {
+            }
+        });
+    }, []);
 
     return (
         <section className="vh-100" >
@@ -41,23 +102,23 @@ export default function SignUp() {
                                     <form>
 
                                         <div className="form-outline mb-4">
-                                            <input type="text" id="form3Example1cg" onChange={(e) => { setUser(e.target.value) }} placeholder="Name" className="form-control form-control-lg" />
+                                            <input type="text" id="form3Example1cg6" onChange={(e) => { setUser(e.target.value) }} placeholder="Name" className="form-control form-control-lg" required />
                                         </div>
 
                                         <div className="form-outline mb-4">
-                                            <input type="text" id="form3Example1cg" onChange={(e) => { setUsername(e.target.value) }} placeholder="Username" className="form-control form-control-lg" />
+                                            <input type="text" id="form3Example1cg" onChange={(e) => { setUsername(e.target.value) }} placeholder="Username" className="form-control form-control-lg" required />
                                         </div>
 
                                         <div className="form-outline mb-4">
-                                            <input type="email" id="form3Example3cg" placeholder="Email" onChange={(e) => { setEmail(e.target.value) }} className="form-control form-control-lg" />
+                                            <input type="email" id="form3Example3cg" placeholder="Email" onChange={(e) => { setEmail(e.target.value) }} className="form-control form-control-lg" required />
                                         </div>
 
                                         <div className="form-outline mb-4">
-                                            <input type="password" id="form3Example4cg" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} className="form-control form-control-lg" />
+                                            <input type="password" id="form3Example4cg" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} className="form-control form-control-lg" required />
                                         </div>
 
                                         <div className="form-outline mb-4">
-                                            <input type="password" id="form3Example4cdg" placeholder="Confirm Password" className="form-control form-control-lg" />
+                                            <input type="password" id="form3Example4cdg" placeholder="Confirm Password" onChange={(e) => { setConfPassword(e.target.value) }} className="form-control form-control-lg" required />
                                         </div>
 
                                         <div className="form-check d-flex justify-content-center mb-5">
@@ -71,7 +132,15 @@ export default function SignUp() {
                                                 I agree all statements in <Link to="/" className="text-body"><u>Terms of service</u></Link>
                                             </label>
                                         </div>
-
+                                        <div className="form-outline d-flex justify-content-center mb-4">
+                                            <Recaptcha
+                                                sitekey="6Lc2rOMcAAAAADZMnatG2s4hlU_gMCNbkEZozThP"
+                                                render="explicit"
+                                                verifyCallback={toggleBot}
+                                                expiredCallback={toggleBot}
+                                                onloadCallback={()=>{}}
+                                            />
+                                        </div>
                                         <div className="d-flex justify-content-center">
                                             <button type="button" onClick={register} className="btn btn-success btn-block btn-lg gradient-custom-4 ">Register</button>
                                         </div>
@@ -80,8 +149,8 @@ export default function SignUp() {
 
                                     </form>
                                     {success ? <>
-                                        <div className="mt-4 alert alert-success" role="alert">
-                                            {message} <Link to="/signin" className="alert-link">Click here to Sign in</Link>
+                                        <div id="alert" className={`mt-4 text-center alert ${isSuccess ? "alert-success" : "alert-danger"}`} role="alert">
+                                            {message} <Link to="/signin" id="signLink" className="alert-link"></Link>
                                         </div>
                                     </> : <></>}
                                 </div>
@@ -91,47 +160,5 @@ export default function SignUp() {
                 </div>
             </div>
         </section>
-        // <div>
-        //     <div className="d-flex align-items-center justify-content-center my-3">
-        //         {/* <div className="bg-white p-56 rounded shadow-2x1 w-1/2"> */}
-        //         <div className="d-flex d-flex-col bg-white rounded-lg dark:bg-gray-800 sm:px-6 md:px-8 lg:w-2/6">
-        //             <h2 className="text-3xl font-bold mb-10 text-gray-800">Create Your Account</h2>
-
-        //             <form id="signupForm" className="space-y-5">
-        //                 <div>
-        //                     <input type="text" placeholder="Name" onChange={(e) => { setUser(e.target.value) }} className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-blue-500" />
-        //                 </div>
-
-        //                 <div>
-        //                     <input type="text" placeholder="Username" onChange={(e) => { setUsername(e.target.value) }} className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-blue-500" />
-        //                 </div>
-
-        //                 <div>
-        //                     <input type="email" placeholder="Email" onChange={(e) => { setEmail(e.target.value) }} className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-blue-500" />
-        //                 </div>
-
-        //                 <div>
-        //                     <input type="password" placeholder="Password" onChange={(e) => { setPassword(e.target.value) }} className="w-full border-2 border-gray-200 p-3 rounded outline-none focus:border-blue-500" />
-        //                 </div>
-
-        //                 <div className="flex items-center">
-        //                     <input type="checkbox" id="agree" />
-        //                     <label htmlFor="agree" className="ml-2 text-gray-700 text-sm">I agree to the terms and privacy.</label>
-        //                 </div>
-
-        //                 <div>
-        //                     <button onClick={register} className="block w-full bg-blue-500 hover:bg-blue-700 p-4 rounded text-white hover:text-while transition duration-300">Sign Up</button>
-        //                 </div>
-        //             </form>
-        //             {success ? <>
-        //                 <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-2 my-5 rounded relative" role="alert">
-        //                     <span className="flex justify-center items-center sm:inline">{message}</span>
-        //                 </div>
-        //                 <Link to="/signin" className="text-blue-700 mx-auto">Click here to Sign In</Link>
-        //             </> : <></>}
-        //         </div>
-        //     </div>
-
-        // </div>
     )
 }
