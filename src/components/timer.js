da3e6eb37be7be5e76980b2acdfad7bce9b7121d
shreya gupta <rupalisahu48@gmail.com>
2021-10-22 21:03:02 +0530

    Merge branch 'main' of https://github.com/ta-verma/Crossword

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import { useStopwatch } from 'react-timer-hook';
+import React from "react";
+import { useStopwatch } from "react-timer-hook";
 
 function MyStopwatch() {
+  //const stopwatchOffset = new Date();
+ // stopwatchOffset.setSeconds(stopwatchOffset.getSeconds() + 300);
   const {
     seconds,
     minutes,
-    hours,
-    days,
+    hours   
   } = useStopwatch({ autoStart: true });
-
-
+  const hourTime = hours < 10 ? `0${hours}` : `${hours}`;
+  const secondTime = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  const minuteTime = minutes < 10 ? `0${minutes}` : `${minutes}`;
   return (
-    <div style={{textAlign: 'center'}}>
+    <div style={{ textAlign: "center" }}>
       <h1>Time Elapsed</h1>
-      <div style={{fontSize: '100px'}}>
-        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+      <div style={{ fontSize: "100px" }}>
+        <span>{hourTime}</span>:<span>{minuteTime}</span>:
+        <span>{secondTime}</span>
       </div>
     </div>
   );
 }
 
-export default function Timer() {
+export default function App() {
   return (
     <div>
       <MyStopwatch />
